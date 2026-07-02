9898aa56a21b3e9ca5b1a613bb681b6bae6785f3
Vincent Garnier <forxer@gmail.com>
2026-07-02 10:46:02 +0000

    chore: scaffold package (composer, pint, rector, phpunit)

---
+++ rector.php
@@ -0,0 +1,68 @@
+<?php
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use RectorLaravel\Rector\Class_\UnifyModelDatesWithCastsRector;
+use RectorLaravel\Rector\FuncCall\RemoveDumpDataDeadCodeRector;
+use RectorLaravel\Rector\MethodCall\EloquentWhereRelationTypeHintingParameterRector;
+use RectorLaravel\Rector\MethodCall\EloquentWhereTypeHintClosureParameterRector;
+use RectorLaravel\Rector\MethodCall\RedirectBackToBackHelperRector;
+use RectorLaravel\Rector\MethodCall\ReplaceServiceContainerCallArgRector;
+use RectorLaravel\Rector\MethodCall\ReverseConditionableMethodCallRector;
+use RectorLaravel\Rector\MethodCall\ValidationRuleArrayStringValueToArrayRector;
+use RectorLaravel\Rector\PropertyFetch\OptionalToNullsafeOperatorRector;
+use RectorLaravel\Rector\StaticCall\RouteActionCallableRector;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withImportNames()
+    ->withParallel(
+        timeoutSeconds: 320,
+        maxNumberOfProcess: 16,
+        jobSize: 20,
+    )
+    ->withCache(
+        cacheClass: FileCacheStorage::class,
+        cacheDirectory: __DIR__.'/.rector_cache',
+    )
+    ->withPaths([
+        __DIR__.'/src',
+    ])
+
+    // Up from PHP 5.3 to 8.4
+    // ->withPhpSets()
+
+    // only PHP 8.4
+    ->withPhpSets(php84: true)
+
+    ->withSkip([
+        // Cet attribut natif PHP n'est pas très utile ;
+        // mieux vaux se baser sur de l'analyse statique
+        // En plus, lors de la rédaction de ce message,
+        // la règle fonctionne mal, elle est buguée...
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
+    ->withRules([
+        EloquentWhereRelationTypeHintingParameterRector::class,
+        EloquentWhereTypeHintClosureParameterRector::class,
+        OptionalToNullsafeOperatorRector::class,
+        RemoveDumpDataDeadCodeRector::class,
+        RedirectBackToBackHelperRector::class,
+        ReplaceServiceContainerCallArgRector::class,
+        ReverseConditionableMethodCallRector::class,
+        RouteActionCallableRector::class,
+        UnifyModelDatesWithCastsRector::class,
+        ValidationRuleArrayStringValueToArrayRector::class,
+    ])
+    ->withSets([
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        instanceOf: true,
+        earlyReturn: true,
+    );
